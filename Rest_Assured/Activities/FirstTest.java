34b8482ffbdabb6b61b23402b6cf08f1f5fddf08
Jivan-Kajave <103193579+Jivan-Kajave@users.noreply.github.com>
2024-06-14 14:54:33 +0530

    Add files via upload

--- Rest_Assured/Activities/FirstTest.java
+++ Rest_Assured/Activities/FirstTest.java
@@ -1,14 +1,57 @@
 package examples;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.response.Response;
 import static org.hamcrest.Matchers.equalTo;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.hamcrest.CoreMatchers;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import java.util.HashMap;
+import java.util.Map;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 public class FirstTest {
     // GET https://petstore.swagger.io/v2/pet/findByStatus?status=alive
-    @Test
+    RequestSpecification requestSpec;
+    ResponseSpecification responseSpec;
+
+    @BeforeClass
+    public void setUp(){
+        //RequestSpecification
+        requestSpec = new RequestSpecBuilder()
+                // Set base URL
+                .setBaseUri("https://petstore.swagger.io/v2/pet")
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        //ResponseSpecification
+        responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectBody("status", CoreMatchers.equalTo("alive"))
+                .expectResponseTime(lessThanOrEqualTo(3000L))
+                .build();
+    }
+
+    // POST https://petstore.swagger.io/v2/pet
+    @Test (priority=1)
+    public void postRequest(){
+        //Create Request Body
+        Map<String, Object> reqBody = new HashMap<>();
+        reqBody.put("id", "4001");
+        reqBody.put("name", "Simba");
+        reqBody.put("status", "alive");
+        //Save the response
+        Response response= given().spec(requestSpec).body(reqBody).when().post();
+        //Assertions
+        response.then().spec(responseSpec);
+    }
+
+    @Test (priority=2)
     public void getRequestWithQueryParam(){
         //Create a baseURI
         Response response=
@@ -34,19 +77,19 @@ public class FirstTest {
     }
 
     // GET https://petstore.swagger.io/v2/pet/{petId}
-    @Test
+    @Test (priority=3)
     public void getRequestWithPathParam() {
         given().//Request Specifications
                 baseUri("https://petstore.swagger.io/v2/pet").
                 header("Content-Type", "application/json").
-                pathParam("petId", 1002).
+                pathParam("petId", 4001).
                 log().all().
 
         when().
                 get("/{petId}").
         then(). //Response Specifications
             statusCode(200).
-            body("name",equalTo("Mufasa")).
+            body("name",equalTo("Simba")).
             body("status",equalTo("alive")).
             log().all();
 
